3c15b714b4843b80686bd71058bdce5d0fb017a2
Yong Wang <yongwang@Yongs-MacBook-Pro.local>
2018-06-26 11:05:41 -0500

    Add configuration for ivy, company

--- init.el
+++ init.el
@@ -36,8 +36,7 @@
 (use-package zenburn-theme
   :ensure t
   :config
-  (load-theme 'zenburn t)
-  )
+  (load-theme 'zenburn t))
 
 ;;; Disable backup of files
 (setq make-backup-files nil)
@@ -52,11 +51,12 @@
 (use-package org
   :ensure t
   :mode ("\\.org\\'" . org-mode)
-  :bind (("C-c l" . org-store-link)
-	 ("C-c a" . org-agenda)
-	 ("C-c c" . org-capture)
-	 ("C-c b" . org-iswitchb)
-	 ("C-c C-w" . org-refile))
+  :bind
+  ("C-c l" . org-store-link)
+  ("C-c a" . org-agenda)
+  ("C-c c" . org-capture)
+  ("C-c b" . org-iswitchb)
+  ("C-c C-w" . org-refile)
   
   :config
   (progn
@@ -69,23 +69,53 @@
              "* TODO %?\n%i\n%a")
             ("j" "Journal" entry (file+olp+datetree "journal.org")
              "* %?\nEntered on %U\n%i\n%a")))
-    (setq org-todo-keywords
-      '((sequence "TODO" "WAIT" "PROJ" "|" "DONE" "CACL")))
-    ;; Structure completion elements
-    ;; The value gets inserted after typing '<' followed by the key
-    ;; and then  the completion key, usually 'TAB'
+    ;; Structure completion elements   
     (add-to-list 'org-structure-template-alist
 		 (list "m" (concat "#+TITLE: ?\n"
 				   "#+AUTHOR: Wang, Yong\n"
 				   "#+STARTUP: content\n"
 				   "#+STARTUP: indent\n")))
-    )
-  )
+    (setq org-todo-keywords
+	  '((sequence "TODO" "WAIT" "PROJ" "|" "DONE" "CACL")))))
 
 ;; org bullets
 (use-package org-bullets
   :ensure t
   :init
   (add-hook 'org-mode-hook
-	    (lambda () (org-bullets-mode +1)))
-  )
+	    (lambda () (org-bullets-mode +1))))
+
+;;; ivy
+(use-package ivy
+  :ensure t
+  :init
+  (setq ivy-use-virtual-buffers nil)
+  (setq ivy-count-format "(%d/%d)")
+  :diminish ivy-mode
+  :bind
+  ("C-c C-r" . ivy-resume)
+  :config
+  (ivy-mode +1))
+;; swiper
+(use-package swiper
+  :ensure t
+  :bind
+  ("C-s" . swiper))
+;; counsel
+(use-package counsel
+  :ensure t
+  :bind
+  ("M-x" . counsel-M-x)
+  ("C-x C-f" . counsel-find-file)
+  ("C-h f" . counsel-describe-function)
+  ("C-h v" . counsel-describe-variable))
+
+;;; company
+(use-package company
+  :ensure t
+  :init (global-company-mode)
+  :config
+  (progn
+    (setq company-tooltip-align-annotations t)
+    (setq comany-show-numbers t))
+  :diminish company-mode)
